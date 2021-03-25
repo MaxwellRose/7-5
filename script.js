489de811783ddb26b05b917b21522bff6f719afe
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-25 13:23:47 +0000

    lots

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let day = 'saturday'
 
 document.getElementById('button').addEventListener('click', input)
 function input () {
-   userinput = document.getElementById('num').value
-   day = document.getElementById('day').value
-   if (userinput <= 18 && (day != 'saturday' && day != 'sunday')) {
+  userinput = document.getElementById('num').value
+  day = document.getElementById('day').value
+  if (userinput <= 18 && (day !== 'saturday' && day !== 'sunday')) {
    alert('Have fun at school')
-  } else if (userinput > 18 && (day != 'saturday' && day !='sunday')) {
-   alert('Have fun at work')
-  }
+  } else if (userinput > 18 && (day !== 'saturday' && day !== 'sunday')) {
+    alert('Have fun at work')}
   else {
     alert('Its the weekend')
   }
